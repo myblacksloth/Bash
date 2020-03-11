0868799792e5209d1e7f25c8d40d3c8eca157da0
Antonio Maulucci <antonio.maulucci@libero.it>
2020-03-11 11:38:43 +0100

    Add files via upload

---
+++ algebra.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# (C) Antonio Maulucci 2020
+
+# downloaded  downloading  script  file
+
+# put urls to video inside "file"
+
+### $ bash script file
+
+##########################
+# i link cambiamo piu' volte nel corso del tempo
+##########################
+# cd downloadvideo/algebra2/
+# put algebra.tgz
+# tar -xvzf algebra.tgz
+##########################
+# screen [-S ${session_name}]
+# screen -ls
+# screen -r ${session_id}
+# control ++ a ++ d # to detach
+# <or> exit to exit
+##########################
+
+# ========================================== >>>>>>>>>
+i=1 # video index [use 1 if 1st video]
+materia="algebra"
+# ========================================== <<<<<<<<<
+
+while read -r urlpage
+do
+	grep -q ${urlpage} '#' # returns 0 if ok or not 0 if not ok
+	teststringa=$?
+	# echo "teststringa= ${teststringa}"
+	# read x
+	if (( $teststringa != 0 )); then
+		wget -P ./downloading/ $urlpage
+		testdownload=$?
+		if (( $testdownload != 0 )); then
+			echo "========= ERROR =========" >> logfile.txt
+			echo ${i} >> logfile.txt
+			echo ${urlpage} >> logfile.txt
+			echo "=========================" >> logfile.txt
+		else
+			echo "downloaded ${i}" >> logfile.txt
+		fi
+		cd ./downloading/
+		filename=$(ls | awk '{print $1}') # si presume che nella directory vi sia solo il file appena scaricato
+		echo "reneaming ${filename} to ${materia}-${i}.mp4" >> logfile.txt
+		mv "${filename}" "${materia}-${i}.mp4"
+		exitcode=$?
+		if (( $exitcode != 0 )); then
+			echo "===== error while reneaming ${filename} ... ${i}"
+		fi
+		mv * ../downloaded/
+		exitcode=$?
+		if (( $exitcode == 0 )); then
+			echo "${materia}-${i}.mp4 moved to downloaded" >> logfile.txt
+		else
+			echo "===== error while moving ${materia}-${i}.mp4" >> logfile.txt
+		fi
+		cd ..
+		((i+=1))
+	fi
+	##############################
+done < $1
